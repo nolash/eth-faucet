962506aa3a7ea19f973bb4150245da71bda9431c
lash <dev@holbrook.no>
2023-02-03 11:41:48 +0000

    Add tests

---
+++ python/tests/test_basic.py
@@ -0,0 +1,131 @@
+# standard imports
+import os
+import unittest
+import logging
+
+# external imports
+from chainlib.connection import RPCConnection
+from chainlib.eth.nonce import RPCNonceOracle
+from chainlib.eth.unittest.ethtester import EthTesterCase
+from chainlib.eth.tx import receipt
+from chainlib.eth.address import to_checksum_address
+from chainlib.eth.gas import balance
+from chainlib.eth.gas import Gas
+from chainlib.eth.gas import OverrideGasOracle
+
+# local imports
+from eth_faucet.faucet import EthFaucet
+
+logging.basicConfig(level=logging.DEBUG)
+logg = logging.getLogger()
+
+
+class TestFaucet(EthTesterCase):
+
+    def setUp(self):
+        super(TestFaucet, self).setUp()
+        self.conn = RPCConnection.connect(self.chain_spec, 'default')
+        nonce_oracle = RPCNonceOracle(self.accounts[0], self.conn)
+        c = EthFaucet(self.chain_spec, signer=self.signer, nonce_oracle=nonce_oracle)
+        (tx_hash_hex, o) = c.constructor(self.accounts[0])
+        r = self.conn.do(o)
+        
+        o = receipt(r)
+        r = self.conn.do(o)
+        self.address = to_checksum_address(r['contract_address'])
+        logg.debug('faucet contract {}'.format(self.address))
+
+
+    def test_basic(self):
+        nonce_oracle = RPCNonceOracle(self.accounts[0], self.conn)
+        c = EthFaucet(self.chain_spec, signer=self.signer, nonce_oracle=nonce_oracle)
+        (tx_hash_hex, o) = c.give_to(self.address, self.accounts[0], self.accounts[2])
+        self.conn.do(o)
+
+        o = balance(self.accounts[9])
+        r = self.conn.do(o)
+        prebalance = int(r, 16)
+
+        o = receipt(tx_hash_hex)
+        r = self.conn.do(o)
+        self.assertEqual(r['status'], 1)
+
+        o = balance(self.accounts[2])
+        r = self.conn.do(o)
+        self.assertEqual(int(r, 16), prebalance)
+
+        (tx_hash, o) = c.set_amount(self.address, self.accounts[0], 1000)
+        r = self.conn.do(o)
+
+        o = receipt(tx_hash_hex)
+        r = self.conn.do(o)
+        self.assertEqual(r['status'], 1)
+
+        (tx_hash_hex, o) = c.give_to(self.address, self.accounts[0], self.accounts[2])
+        self.conn.do(o)
+
+        o = receipt(tx_hash_hex)
+        r = self.conn.do(o)
+        self.assertEqual(r['status'], 0)
+
+        contract_gas_oracle = OverrideGasOracle(limit=21055, conn=self.conn)
+        cg = Gas(self.chain_spec, signer=self.signer, nonce_oracle=nonce_oracle, gas_oracle=contract_gas_oracle)
+        (tx_hash_hex, o) = cg.create(self.accounts[0], self.address, 1000)
+        self.conn.do(o)
+
+        o = receipt(tx_hash_hex)
+        r = self.conn.do(o)
+        self.assertEqual(r['status'], 1)
+        
+        (tx_hash_hex, o) = c.give_to(self.address, self.accounts[0], self.accounts[2])
+        self.conn.do(o)
+
+        o = receipt(tx_hash_hex)
+        r = self.conn.do(o)
+        self.assertEqual(r['status'], 1)
+
+        o = balance(self.accounts[2])
+        r = self.conn.do(o)
+        self.assertEqual(int(r, 16), prebalance + 1000)
+
+
+    def test_basic(self):
+        nonce_oracle = RPCNonceOracle(self.accounts[0], self.conn)
+        c = EthFaucet(self.chain_spec, signer=self.signer, nonce_oracle=nonce_oracle)
+
+        contract_gas_oracle = OverrideGasOracle(limit=21055, conn=self.conn)
+        cg = Gas(self.chain_spec, signer=self.signer, nonce_oracle=nonce_oracle, gas_oracle=contract_gas_oracle)
+        (tx_hash_hex, o) = cg.create(self.accounts[0], self.address, 1000)
+        self.conn.do(o)
+
+        o = receipt(tx_hash_hex)
+        r = self.conn.do(o)
+        self.assertEqual(r['status'], 1)
+
+        (tx_hash, o) = c.set_amount(self.address, self.accounts[0], 1000)
+        r = self.conn.do(o)
+
+        o = balance(self.accounts[1])
+        r = self.conn.do(o)
+        prebalance = int(r, 16)
+
+        nonce_oracle = RPCNonceOracle(self.accounts[1], self.conn)
+        gas_price = 1000000000
+        gas_oracle = OverrideGasOracle(limit=100000, price=gas_price, conn=self.conn)
+        c = EthFaucet(self.chain_spec, signer=self.signer, nonce_oracle=nonce_oracle, gas_oracle=gas_oracle)
+        (tx_hash_hex, o) = c.gimme(self.address, self.accounts[1])
+        self.conn.do(o)
+
+        o = receipt(tx_hash_hex)
+        r = self.conn.do(o)
+        logg.debug('rrrr {}'.format(r))
+        cost = r['gas_used'] * gas_price
+        self.assertEqual(r['status'], 1)
+
+        o = balance(self.accounts[1])
+        r = self.conn.do(o)
+        self.assertEqual(int(r, 16), prebalance - cost + 1000)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- solidity/EthFaucet.sol
+++ solidity/EthFaucet.sol
@@ -8,16 +8,16 @@ contract EthFacuet {
 	address public registry;
 	address public periodChecker;
 	uint256 public sealState;
-	uint256 public value;
+	uint256 public amount;
 
 	uint8 constant REGISTRY_STATE = 1;
 	uint8 constant PERIODCHECKER_STATE = 2;
 	uint8 constant VALUE_STATE = 4;
 	uint256 constant public maxSealState = 7;
 
-	event FaucetUsed(address indexed _recipient, address indexed _token, uint256 _value);
-	event FaucetFail(address indexed _recipient, address indexed _token, uint256 _value);
-	event FaucetAmountChange(uint256 _value);
+	event FaucetUsed(address indexed _recipient, address indexed _token, uint256 _amount);
+	event FaucetFail(address indexed _recipient, address indexed _token, uint256 _amount);
+	event FaucetAmountChange(uint256 _amount);
 	event FaucetStateChange(uint256 indexed _sealState, address _registry, address _periodChecker);
 
 	constructor() {
@@ -32,12 +32,12 @@ contract EthFacuet {
 		return uint256(sealState);
 	}
 
-	function setValue(uint256 _v) public returns(uint256) {
+	function setAmount(uint256 _v) public returns(uint256) {
 		require(msg.sender == owner, 'ERR_NOT_OWNER');
 		require(sealState & VALUE_STATE == 0, 'ERR_SEALED');
-		value = _v;
-		emit FaucetAmountChange(value);
-		return value;
+		amount = _v;
+		emit FaucetAmountChange(amount);
+		return amount;
 	}
 
 	function setPeriodChecker(address _checker) public {
@@ -74,11 +74,11 @@ contract EthFacuet {
 
 		(_ok, _result) = registry.call(abi.encodeWithSignature("have(address)", _recipient));
 		if (!_ok) {
-			emit FaucetFail(_recipient, address(0), value);
+			emit FaucetFail(_recipient, address(0), amount);
 			revert('ERR_TRANSFER');
 		}
 			
-		emit FaucetUsed(_recipient, address(0), value);
+		emit FaucetUsed(_recipient, address(0), amount);
 		return true;
 	}
 
@@ -93,16 +93,16 @@ contract EthFacuet {
 	}
 
 
-	function get() public returns(uint256) {
-		check(msg.sender);
-		payable(msg.sender).transfer(value);
-		return value;
+	function gimme() public returns(uint256) {
+		require(check(msg.sender));
+		payable(msg.sender).transfer(amount);
+		return amount;
 	}
 
-	function to(address _recipient) public returns(uint256) {
-		check(_recipient);
-		payable(_recipient).transfer(value);
-		return value;
+	function giveTo(address _recipient) public returns(uint256) {
+		require(check(_recipient));
+		payable(_recipient).transfer(amount);
+		return amount;
 	}
 
 	receive () payable external {
