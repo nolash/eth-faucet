f9563b7a0002513f9c34dc246cafda900f96993d
lash <dev@holbrook.no>
2023-02-11 10:42:06 +0000

    Add codegen script

---
+++ python/eth_faucet/runnable/gen.py
@@ -0,0 +1,22 @@
+# standard imports
+import sys
+import os
+import argparse
+
+script_dir=os.path.dirname(os.path.realpath(__file__))
+data_dir=os.path.join(script_dir, '..', 'data')
+
+tr={
+        'faucet': 'EthFaucet',
+        'period': 'PeriodSimple',
+        }
+
+argparser = argparse.ArgumentParser()
+argparser.add_argument('name', type=str, default='faucet', choices=['faucet', 'period'], help='list code identifiers')
+args = argparser.parse_args(sys.argv[1:])
+
+fp = os.path.join(data_dir, tr[args.name] + '.bin')
+f = open(fp, 'r')
+r = f.read()
+f.close()
+print(r)
